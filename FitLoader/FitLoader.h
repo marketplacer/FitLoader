f79ba7bb1b1654412b6dfb6488dae472a07779ce
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-06-11 16:01:49 +1000

    Initial Commit

---
+++ FitLoader/FitLoader.h
@@ -0,0 +1,19 @@
+//
+//  FitLoader.h
+//  FitLoader
+//
+//  Created by Evgenii on 11/06/2015.
+//  Copyright (c) 2015 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FitLoader.
+FOUNDATION_EXPORT double FitLoaderVersionNumber;
+
+//! Project version string for FitLoader.
+FOUNDATION_EXPORT const unsigned char FitLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FitLoader/PublicHeader.h>
+
+
